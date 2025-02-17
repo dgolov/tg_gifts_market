15178c98ae4fff171dd79f36a49ed5d93d1f34b6
Ivan Onin <dmitriy.evgenyevich@gmail.com>
2025-02-17 23:21:55 +0300

    added gift sell

---
+++ src/gifts.py
@@ -0,0 +1,9 @@
+from aiogram.fsm.state import StatesGroup, State
+
+
+class SellGift(StatesGroup):
+    gift_name = State()
+    gift_model = State()
+    gift_background = State()
+    gift_color = State()
+    gift_pattern = State()
