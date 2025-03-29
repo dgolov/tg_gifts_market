057814aad9fe1961529088103ffaeade2a5340a9
Дмитрий Голов <dgolov@mts.ru>
2025-03-29 21:11:21 +0300

    upd models

--- db/models.py
+++ db/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.sql import func
-from db.base import Base
+from db.engine import Base
 
 
 class Gift(Base):
@@ -10,6 +10,7 @@ class Gift(Base):
     user_id = Column(Integer, nullable=False)
     username = Column(String, nullable=False)
     gift_name = Column(String, nullable=False)
+    gift_number = Column(Integer, nullable=True)
     gift_model = Column(String, nullable=False)
     gift_background = Column(String, nullable=True)
     gift_color = Column(String, nullable=True)
@@ -17,3 +18,5 @@ class Gift(Base):
     price = Column(Float, nullable=True)
     created_at = Column(DateTime, default=func.now())
     post_id = Column(Integer, nullable=True)
+
+
